d8f23aad0ee0fcad125ccdb92e56428ae3a3a1e1
picel <picel@picel.net>
2022-02-17 08:15:28 +0000

    [ADD] images, descriptions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,13 @@ class HomeController < ApplicationController
   def index
     if browser.platform.ios?
       @link = "https://apps.apple.com/kr/app/%EC%B0%BD%EB%8C%80%ED%95%99%EC%8B%9D/id1606061004?l=en"
+      @platform = "iOS"
     elsif browser.platform.android?
       @link = "https://play.google.com/store/apps/details?id=com.picel.cwnumeal"
+      @platform = "android"
     else
       @link = "https://flutter.picel.net/cwnumeal"
+      @platform = "web"
     end
   end
   def other
